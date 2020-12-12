4c5427cb4663b59a0213d98f8ba7f7c3558a55e7
babulakterfsd <babulakterfsd@gmail.com>
2020-12-13 01:11:42 +0600

    some missing webpack plugin added

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
       entry: './src/js/index.js',
